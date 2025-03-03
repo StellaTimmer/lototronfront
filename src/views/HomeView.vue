5e54a696317d4fd05b2bda1fcfc0f40df3ccada1
Jaanus <tonisson.jaanus@gmail.com>
2025-03-03 15:36:34 +0200

    First steps

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-4 justify-content-center">
+
+        <div class="row">
+          <p>
+            Tere tulemast lõunalototroni!
+            Ei tea kellega lõunale minna?
+            Pole probleemi
+            Aitame
+          </p>
+        </div>
+
+        <div class="row">
+          <img src="@/assets/toidupilt3.jpg"
+               height="100"
+               width="100"
+               class="img-fluid"
+               alt="toidupilt">
+        </div>
+
+      </div>
+      <div class="col-4 ">
+
+        <div class="row">
+          user INPUT
+        </div>
+        <div class="row">
+          PASSWORD INPUT
+        </div>
+        <div class="row">
+          LOGI SISSE NUPP
+        </div>
+        <div class="row">
+          REGISTREERI NUPP
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
