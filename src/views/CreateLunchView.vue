4becf8946bb87716bdf01f88563261b0ecabf074
StellaTimmer <stella.timmer@gmail.com>
2025-03-05 10:39:47 +0200

    05.03 Create Lunch View WIP

--- src/views/CreateLunchView.vue
+++ src/views/CreateLunchView.vue
@@ -1,5 +1,82 @@
 <template>
 
+  <div>
+
+    <div class="container text-center">
+      <br>
+      <br>
+      <br>
+
+      <div class="row">
+
+        <div> Loo lõuna: </div>
+        <br>
+        <br>
+        <br>
+        <div class="col">
+          <div>
+            Vali kuupäev:
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div>
+            Vali söögikoht:
+          </div>
+
+
+        </div>
+
+
+        <div class="col">
+
+          <div>
+            Vali kellaaeg:
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div>
+            <button type="submit" class="btn btn-warning btn-lg">KINNITA LÕUNA</button>
+
+          </div>
+
+        </div>
+
+
+        <div class="col">
+
+          <div>
+            Sinu lõunad:
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div>
+            Tulemas:
+          </div>
+          <br>
+          <br>
+          <div>
+            Möödunud:
+          </div>
+
+        </div>
+
+        <div class="col">
+          Siia tulevad reklaamid
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+
 </template>
 
 
