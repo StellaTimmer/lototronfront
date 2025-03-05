b7f0f212601b06af67b6526074f0f56f7669c087
katlinkoemets <katlinkoemets@gmail.com>
2025-03-05 09:24:03 +0200

    Registreerimise vaade

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="registration">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col">
+          <RegistrationForm/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import RegistrationForm from "@/components/RegistrationForm.vue";
+
+export default {
+  name: "RegistrationForm",
+  components: {
+    RegistrationForm
+  }
+};
+</script>
+
