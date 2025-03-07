07f46f247fbab12ce2ac16102404b75a38f61ff7
katlinkoemets <katlinkoemets@gmail.com>
2025-03-07 10:07:03 +0200

    Account 0703 WIP

---
+++ src/views/AccountView.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="account-container">
+
+  <!--Vasak veerg: Profiilipilt ja 6 >=""
+
+</div>
+</template>
+
+<script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "AccountView",
+  components: {FontAwesomeIcon}
+}
+</script>
+
+
